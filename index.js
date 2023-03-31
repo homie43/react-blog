8c7cf9101370aab8c34b57cd662a3d1786a98b2f
Vladimir <homie43@yandex.ru>
2023-03-31 14:46:37 +0300

    test authorization

--- index.js
+++ index.js
@@ -2,9 +2,21 @@ import express from "express";
 
 const app = express();
 
+// комманда позволяет читать json который приходит в запросах
+app.use(express.json());
+
 app.get("/", (req, res) => {
-  res.send("sdfsdfsdf Hello World");
+  res.send("Hello World");
 });
+
+app.post("/auth/login", (req, res) => {
+  console.log(req.body);
+  res.json({
+    success: true,
+  });
+});
+
+// отправляем логи и пароль? затем обрабатываем их
 
 // запускаем приложение
 app.listen(4444, (err) => {
