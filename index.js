6dd2033d7bc35a2b3784df0558f1c5879a20b38c
Vladimir <homie43@yandex.ru>
2023-04-07 00:47:29 +0300

    add connect mongoDB and add mongoose

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 import express from "express";
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:wwwwww@cluster0.k8gbj8e.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("DB OK"))
+  .catch((err) => console.log("DB error", err));
 
 const app = express();
 
@@ -12,13 +20,17 @@ app.get("/", (req, res) => {
 
 app.post("/auth/login", (req, res) => {
   console.log(req.body.email);
-  const token = jwt.sign(
-    {
-      email: req.body.email,
-      fullName: "Вася Пупкин",
-    },
-    "secret123"
-  );
+
+  if (req.body.email === "test@test.ru") {
+    const token = jwt.sign(
+      {
+        email: req.body.email,
+        fullName: "Вася Пупкин",
+      },
+      "secret123"
+    );
+  }
+
   res.json({
     success: true,
     token,
