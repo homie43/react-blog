b9d86d9fd86583cc9325c95eb07d8106de199989
Vladimir <homie43@yandex.ru>
2023-05-12 17:35:59 +0300

    create tags controller

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -141,3 +141,22 @@ export const update = async (req, res) => {
     });
   }
 };
+
+// получение тэгов
+export const getLastTags = async (req, res) => {
+  try {
+    const posts = await PostModel.find().limit(5).exec();
+
+    const tags = posts
+      .map((obj) => obj.tags)
+      .flat()
+      .slice(0, 5);
+
+    res.json(tags);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Не удалось получить тэги",
+    });
+  }
+};
